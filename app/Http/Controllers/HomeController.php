edcbc8c0359dc5d465a182f1112cf4e03364c47e
Phong PhanVoTrong <phanvo.trong.phong@gmail.com>
2024-02-23 14:47:27 +0700

    fix ui

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('site/user/home');
+        return view('site/components/user/home');
     }
 }
